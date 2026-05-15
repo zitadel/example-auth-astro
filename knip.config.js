bb9b34cd37dcf856afaa9aad5440cbc534a22d17
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-15 16:14:59 +0300

    fix: align scaffold files with parity standards

--- knip.config.js
+++ knip.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   ignore: ['commitlint.config.js'],
   ignoreDependencies: ['@commitlint/config-conventional'],
+  entry: [
+    'src/pages/**/*',
+    'src/components/**/*',
+    'src/layouts/**/*',
+    'src/lib/**/*',
+  ],
 };
